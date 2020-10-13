f7e8ae69a0db1994168c186e67bc40d30a5758c3
Amon BAZONGO <amonmoce@yanfoma.tech>
2020-10-13 21:49:38 +0800

    modification for lab class

--- scripts/init-configserver.js
+++ scripts/init-configserver.js
@@ -6,7 +6,7 @@ rs.initiate(
       members: [
          { _id: 0, host : "config01:27017" },
          { _id: 1, host : "config02:27017" },
-         { _id: 2, host : "config03:27017" }
+         // { _id: 2, host : "config03:27017" }
       ]
    }
 )
